844f4a9ec13b6e119b4fd8776ea0b08456f73980
Alexander Barkume <alexbarkume@dyn-160-39-224-214.dyn.columbia.edu>
2015-11-13 04:50:53 -0500

    Forgot to add auctions. Whoops.

---
+++ firstapp/app/models/Auctions.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.*;
+
+@Entity
+public class Auctions extends Model {
+    
+    @Id
+    public Long aucId;
+    
+    
+    public Long artid;
+    public String openDate;
+    public String closeDate;
+    public Long bidCount;
+    public Long currentBid;
+    
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    public Artworks artwork;
+    
+    
+    
+}
