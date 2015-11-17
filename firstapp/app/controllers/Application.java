d41f32761f87bf98c96f21c2589d501b2b3a7132
joshfram <jpf2141@columbia.edu>
2015-11-17 00:53:30 -0500

    Modals totally automated, still small bugs in bidding and upvoting

--- firstapp/app/controllers/Application.java
+++ firstapp/app/controllers/Application.java
@@ -28,7 +28,7 @@ public class Application extends Controller {
        // for(int i=0;i<artworksT.size();i++){
       //      System.out.println(artworksT.get(i).filePath);
        // } 
-         return ok(index.render(arts.get(0),arts.get(1),arts.get(3),arts.get(4),arts.get(5),arts.get(6),arts.get(7),arts.get(0),arts.get(0),Form.form(Index.class)));
+         return ok(index.render(arts.get(0),arts.get(1),arts.get(2),arts.get(3),arts.get(4),arts.get(5),arts.get(6),arts.get(7),arts.get(8),Form.form(Index.class)));
      }
 
     public Result login() {
